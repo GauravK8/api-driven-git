6b4271f9e67dd77262e8fa40ccbae968bbd85b68
Gaurav <gauravkapadiya8@gmail.com>
2024-09-04 23:26:19 +0530

    Add compression and helmet
    Also added unhandled exception handler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config';
 import express, { Application } from 'express';
+import compression from 'compression';
+import helmet from 'helmet';
 import { router } from '@/routes';
 import cors from 'cors';
 import logger from './utils/logger';
@@ -7,7 +9,23 @@ import logger from './utils/logger';
 const app: Application = express();
 const port = process.env.PORT;
 
+process.on('uncaughtException', function (err) {
+  console.error(err.stack);
+});
+
+process.on('SIGTERM', (signal) => {
+  console.log(`Process ${process.pid} received a SIGTERM signal`);
+  process.exit(0);
+});
+
+process.on('SIGINT', (signal) => {
+  console.log(`Process ${process.pid} has been interrupted`);
+  process.exit(0);
+});
+
 app.use(cors());
+app.use(compression());
+app.use(helmet());
 app.disable('x-powered-by');
 app.use(express.json());
 app.use(router);
